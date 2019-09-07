261405f1b1881493f78b4012face76d229cdbdb8
mkkiyoi <mkkiyoi@gmail.com>
2019-09-06 17:19:40 -0700

    Added comments to the first half of game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,61 +1,141 @@
 (function () {
+
     'use strict';
 
+    // Global variable containing the maximum number of guesses allowed in each game.
     var MAX_GUESSES = 12;
 
+    // Game object. Contains various properties and game logic to run the word guess game.
     var game = {
+
+        /* ------------------ Game Object Properties ------------------ */
+
+        // Array containing all letters of the English alphabet
         letters: ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+        
+        // Store current guesses left
         guessesLeft: MAX_GUESSES,
+
+        // Array of words that can be guessed.
         words: ['apple', 'banana', 'orange', 'strawberry', 'blueberry', 'blackberry', 'grapes'],
+
+        // Words already used in the game.
         previousWords: [],
+
+        // Current word being guessed.
         wordChosen: '',
+
+        // Array which holds both placeholder '-' and letters already guessed in the word.
+        // Used to display the word in '- - - - - -' format.
         currentWord: [],
+
+        // Array containing the letters already guessed by the player.
         lettersGuessed: [],
+
+        // Tracks number of wins.
         wins: 0,
+
+        // Array contains the images associated with the words being guessed.
+        // Used to display images at the end of each game.
         images: [],
 
+        /* ------------------ Game Object Methods ------------------ */
+
+        /* Resets the necessary html content and game properties to begin a new game. */
         resetGame: function() {
-            // Reset variables
+
+            // Reset game object properties
             this.guessesLeft = MAX_GUESSES;
             this.wordChosen = this.words[Math.floor(Math.random() * this.words.length)];
             this.currentWord = [];
             this.lettersGuessed = [];
 
+            // Reset displayed word to be of form '- - - - -'
             for (var i = 0; i < this.wordChosen.length; i++) {
                 this.currentWord.push('-');
             }
+
+            // Displays the newly chosen word in the form '- - - - -'
+            this.displayWordGuess();
+
+            // Resets the html displaying letters already guessed.
             document.getElementById('letters-guessed').innerHTML = '';
+
+            // Resets the html displaying number of guesses left.
             this.updateGuessesRemaining(MAX_GUESSES);
-            this.displayWordGuess();
+
         },
 
+        /* Core method to make a guess of a letter the user has chosen. */
         guess: function(event) {
+
+            // Get the key that was pressed.
             var letter = event.key;
+
+            // Check if the key pressed is a letter and if it was guessed yet.
             if (this.letters.includes(letter) && !this.lettersGuessed.includes(letter)) {
+
+                // Update letters that have been pressed and display newly used letter.
                 this.updateLettersGuessed(letter);
+
+                // Check if the letter is in the chosen word.
                 if (this.wordChosen.includes(letter)) {
+
+                    // Update the letters guessed correctly and update HTML accordingly.
                     this.updateWordGuess(letter);
+
                 }
+
+                // Decrement the bumber of guesses left by 1.
                 this.guessesLeft -= 1;
+
+                // Update HTML showing how many guesses the player has left.
                 this.updateGuessesRemaining(this.guessesLeft);
+
             }
+            // Check if the game is over
             if (this.isGameOver()) {
+
+                // Start new game since the current game is over.
                 this.resetGame();
+
             }
+
         },
 
+        /* Updates the HTML showing how many guesses the player has left. */
         updateGuessesRemaining: function(guesses) {
+
+            // Change the innerHTML to reflect the remaining guess.
             document.getElementById('guesses-remaining').innerHTML = guesses;
+
         },
 
+        /* 
+            Updates the array containing the letters that were guessed correctly and
+            letters yet to be guessed. 
+        */
         updateWordGuess: function(letter) {
+            
+            // Get length of the chosen word. 
             var length = this.wordChosen.length;
+
+            // Update correctly guessed letters to be displayed.
             for (var i = 0; i < length; i++) {
+
+                // Check if the letter guess matches the letter in the word at index i.
                 if (this.wordChosen.charAt(i) === letter) {
+
+                    // Update the letter at index i in currentWord to the guessed letter.
                     this.currentWord[i] = letter;
+
                 }
+
             }
+
+            // Update HTML to reflect newly guessed letter.
             this.displayWordGuess();
+
         },
 
         displayWordGuess: function () {
